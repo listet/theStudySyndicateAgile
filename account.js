650145d91f6515ec58a9b52a17a121c02613329e
Lisa Tetzlaff <lisa.emma.tetzlaff@gmail.com>
2024-03-19 12:10:41 +0100

    first commit

--- account.js
+++ account.js
@@ -1,7 +1,17 @@
 'use strict'
 
+document.addEventListener('DOMContentLoaded', function () {
+    initContentLogin();
+});
+
+
+function initContentLogin() {
+    document.querySelector('#formContainerLogin').classList.remove('d-none');
+    document.querySelector('#formContainerRegister').classList.add('d-none');
+}
+
 window.addEventListener('load', async () => {
-    const loginForm = document.querySelector('form');
+    const loginForm = document.querySelector('#loginForm');
     loginForm.addEventListener('submit', async (event) => {
         event.preventDefault();
 
@@ -14,25 +24,31 @@ window.addEventListener('load', async () => {
             return;
         }
 
-            try {
-                const userData = await fetchUserData();
-                const user = userData.users.find(user => user.username === username && user.password === password);
+        try {
+            const userData = await fetchUserData();
+            const user = userData.users.find(user => user.username === username && user.password === password);
 
-                if (!user) {
-                    alert('Ogiltig användernamn eller lösenord.');
-                    return;
-                }
-                const role = user.role;
-                if (role === 'admin') {
-                    window.location.href = 'profile.html' // ADMIN lägg till rätt html länk här!
-                } else {
-                    window.location.href = 'profile.html' // USER lägg till rätt html länk här!
-                }
-            } catch (error) {
-                alert('Login misslyckades', error);
+            if (!user) {
+                alert('Ogiltig användernamn eller lösenord.');
+                return;
             }
-        });
+            const role = user.role;
+            if (role === 'admin') {
+                window.location.href = 'profile.html' // ADMIN lägg till rätt html länk här!
+            } else {
+                window.location.href = 'profile.html' // USER lägg till rätt html länk här!
+            }
+        } catch (error) {
+            alert('Login misslyckades', error);
+        }
     });
+
+    // Event listener för registerBtn
+    const registerBtn = document.querySelector('#registerBtn');
+    registerBtn.addEventListener('click', () => {
+        initContentRegister();
+    });
+});
 
 async function fetchUserData() {
     try {
@@ -48,3 +64,12 @@ async function fetchUserData() {
     }
 }
 
+function initContentRegister() {
+    console.log('registerForm')
+    document.querySelector('#formContainerLogin').classList.add('d-none');
+    document.querySelector('#formContainerRegister').classList.remove('d-none');
+}
+
+// function - getUsers
+
+// function - addUser
